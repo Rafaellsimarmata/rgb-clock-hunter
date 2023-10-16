016ceb1e9381914337f4984b08444a18c6219f8d
Rafael Simarmata <80381531+Rafaellsimarmata@users.noreply.github.com>
2023-10-16 20:33:44 +0700

    version final

--- rgb-clock-test.cpp
+++ rgb-clock-test.cpp
@@ -5,20 +5,80 @@
 using namespace std;
 
 unsigned int program;
+GLint color1Loc;
+GLint color2Loc;
+GLint color3Loc;
 
-float velocitySecondPointer = -6;
-float velocityMinutePointer = -0.1;
-float velocityHourPointer = -0.00417;
+float velocitySecondPointer = -6.0f /2;  // 360 derajat / 60 detik  
+float velocityMinutePointer = -0.1f / 2; // 360 derajat / 3600 detik
+float velocityHourPointer = -0.00834f / 2; // 360 derajat / 86 400 detik * 2
 
-float currentSecondPointer = -188.4f;
-float currentMinutePointer = -188.4f;
-float currentHourPointer = -188.4f;
+float currentSecondPointer = 270.0f;   
+float currentMinutePointer = 270.0f;
+float currentHourPointer = 270.0f;
 
 float lastTime = 0.0f;
 float currentTime = 0.0f;
 float deltaTime = 0.0f;
 
 float speed = 1;
+float color1[] = { 1.0, 0.0, 0.0, 1.0 };
+float color2[] = { 0.0, 1.0, 0.0, 1.0 };
+float color3[] = { 0.0, 0.0, 1.0, 1.0 };
+
+void buttonFunction(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+	if (key == GLFW_KEY_UP && action == GLFW_PRESS) {
+		speed *= 2;
+		cout << "key up pressed, speed = " << speed << endl;
+	}
+	if (key == GLFW_KEY_SPACE && action == GLFW_PRESS) {
+		speed = 1;
+		cout << "key Space pressed, speed = " << speed << endl;
+	}
+	if (key == GLFW_KEY_C && action == GLFW_PRESS) {
+		for (int i = 0; i < 4; i++)
+		{
+			color1[i] = (rand() * 1.0f) / RAND_MAX;
+			color2[i] = (rand() * 1.0f) / RAND_MAX;
+			color3[i] = (rand() * 1.0f) / RAND_MAX;
+		}
+		glProgramUniform4f(program, color1Loc, color1[0], color1[1], color1[2], color1[3]);
+		glProgramUniform4f(program, color2Loc, color2[0], color2[1], color2[2], color2[3]);
+		glProgramUniform4f(program, color3Loc, color3[0], color3[1], color3[2], color3[3]);
+		cout << "key C pressed, Color Change "<< endl;
+	}
+	if (key == GLFW_KEY_L && action == GLFW_PRESS) {
+		glfwSetTime(currentTime + 3600);
+		cout << "key L pressed, Fast Forwarding 1 Hour" << endl;
+	}
+	if (key == GLFW_KEY_J && action == GLFW_PRESS) {
+		glfwSetTime(currentTime - 3600);
+		cout << "key J pressed, Rewinding 1 Hour" << endl;
+	}
+	if (key == GLFW_KEY_I && action == GLFW_PRESS) {
+		glfwSetTime(currentTime + 300);
+		cout << "key I pressed, Fast Forwarding 5 Minutes" << endl;
+	}
+	if (key == GLFW_KEY_K && action == GLFW_PRESS) {
+		glfwSetTime(currentTime - 300);
+		cout << "key K pressed, Rewinding 5 Minutes" << endl;
+	}
+	if (key == GLFW_KEY_X && action == GLFW_PRESS) {
+		float jam, menit;
+		cout << "Jam : "; cin >> jam;
+		cout << endl;
+
+		cout << "Menit : "; cin >> menit;
+		cout << endl;
+
+		float hourInSecond = jam * 3600;
+		float minuteInSecond = menit * 60;
+
+		glfwSetTime(hourInSecond + minuteInSecond);
+		cout << "Time Changed Successfully" << endl;
+	}
+}
 
 int main(void){
 	GLFWwindow* window;
@@ -37,7 +97,7 @@ int main(void){
 
 	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
-
+	glfwSetKeyCallback(window, buttonFunction);
 
 	GLenum err = glewInit();
 
@@ -49,7 +109,8 @@ int main(void){
 	};
 
 	unsigned int position[] = {
-		0, 1, 2, 0, 2, 3
+		0, 1, 2, 
+		0, 2, 3,
 	};
 
 	unsigned int vertexBuffer;
@@ -91,6 +152,15 @@ int main(void){
 	GLint currentSecondPointerLoc = glGetUniformLocation(program, "currentSecondPointer");
 	GLint currentMinutePointerLoc = glGetUniformLocation(program, "currentMinutePointer");
 	GLint currentHourPointerLoc = glGetUniformLocation(program, "currentHourPointer");
+
+	color1Loc = glGetUniformLocation(program, "color1");
+	glProgramUniform4f(program, color1Loc, color1[0], color1[1], color1[2], color1[3]);
+
+	color2Loc = glGetUniformLocation(program, "color2");
+	glProgramUniform4f(program, color2Loc, color2[0], color2[1], color2[2], color2[3]);
+
+	color3Loc = glGetUniformLocation(program, "color3");
+	glProgramUniform4f(program, color3Loc, color3[0], color3[1], color3[2], color3[3]);
 	
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
@@ -99,7 +169,7 @@ int main(void){
 		deltaTime = speed * (currentTime - lastTime);
 		lastTime = currentTime;
 
-		currentSecondPointer += deltaTime * velocitySecondPointer;
+		currentSecondPointer = currentSecondPointer +  (deltaTime * velocitySecondPointer);
 		currentMinutePointer += deltaTime * velocityMinutePointer;
 		currentHourPointer += deltaTime * velocityHourPointer;
 
@@ -107,9 +177,9 @@ int main(void){
 		//cout <<" currentMinutePointer" << currentMinutePointer << endl;
 		//cout <<" currentHourPointer" << currentHourPointer << endl;
 
-		glUniform1f(currentSecondPointerLoc, currentSecondPointer/120.0f);
-		glUniform1f(currentMinutePointerLoc, currentMinutePointer / 120.0f);
-		glUniform1f(currentHourPointerLoc, currentHourPointer / 120.0f);
+		glUniform1f(currentSecondPointerLoc, currentSecondPointer * 3.14159265f / 180.0f );
+		glUniform1f(currentMinutePointerLoc, currentMinutePointer * 3.14159265f / 180.0f);
+		glUniform1f(currentHourPointerLoc, currentHourPointer * 3.14159265f / 180.0f);
 
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
